95a98a562a8d58c89840e418f75f4df5bf177437
ashish choudhary <aashish.choudhary1@gmail.com>
2020-11-13 02:19:00 +0530

    fixing compilation error

--- src/main/java/com/jib/example/spring/Controller.java
+++ src/main/java/com/jib/example/spring/Controller.java
@@ -3,11 +3,11 @@ package com.jib.example.spring;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
-    public class Controller {
-        @RequestMapping("/")
-        public String message() {
+@RestController
+public class Controller {
+    @RequestMapping("/hello")
+    public String message() {
             return "Hello From Spring-Boot Jib";
-        }
+    }
     }
 
--- src/main/java/com/jib/example/spring/SpringbootApplication.java
+++ src/main/java/com/jib/example/spring/SpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.jib.example.springboot;
+package com.jib.example.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
